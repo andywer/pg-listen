064788b9581fc7f4d825a723b4988275df3ea48b
Andy Wermke <andy.dev@next-step-software.com>
2019-05-15 08:50:55 +0200

    Fix re-subscription code and add a test

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,6 +1,11 @@
 import test from "ava"
+import DebugLogger from "debug"
 import createPostgresSubscriber, { PgParsedNotification } from "../src/index"
 
+// Need to require `pg` like this to avoid ugly error message
+import pg = require("pg")
+
+const debug = DebugLogger("pg-listen:test")
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 test("can connect", async t => {
@@ -38,6 +43,59 @@ test("can listen and notify", async t => {
       { hello: "world" }
     ])
   } finally {
+    await hub.close()
+  }
+})
+
+test("getting notified after connection is terminated", async t => {
+  const notifications: PgParsedNotification[] = []
+  const receivedPayloads: any[] = []
+  let reconnects = 0
+
+  const connectionString = "postgres://postgres:postgres@localhost:5432/postgres"
+  let client = new pg.Client({ connectionString })
+  await client.connect()
+
+  const hub = createPostgresSubscriber(
+    { connectionString: connectionString + "?ApplicationName=pg-listen-termination-test" },
+    { paranoidChecking: 1000 }
+  )
+  await hub.connect()
+
+  try {
+    await hub.listenTo("test")
+    hub.events.on("notification", (notification: PgParsedNotification) => notifications.push(notification))
+    hub.events.on("reconnect", () => reconnects++)
+    hub.notifications.on("test", (payload: any) => receivedPayloads.push(payload))
+
+    await delay(1000)
+    debug("Terminating database backend")
+
+    // Don't await as we kill some other connection, so the promise won't resolve (I think)
+    client.query("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND usename = current_user")
+    await delay(2000)
+
+    client = new pg.Client({ connectionString })
+    await client.connect()
+
+    debug("Sending notification...")
+    await client.query(`NOTIFY test, '{"hello": "world"}';`)
+    await delay(500)
+
+    t.deepEqual(hub.getSubscribedChannels(), ["test"])
+    t.deepEqual(notifications, [
+      {
+        channel: "test",
+        payload: { hello: "world" },
+        processId: notifications[0] ? notifications[0].processId : 0
+      }
+    ])
+    t.deepEqual(receivedPayloads, [
+      { hello: "world" }
+    ])
+    t.is(reconnects, 1)
+  } finally {
+    debug("Closing the subscriber")
     await hub.close()
   }
 })
